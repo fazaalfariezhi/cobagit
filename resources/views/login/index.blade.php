e1f55645f67616143486f7f468e6d11b5f738da0
fazaalfariezhi <fazaalfariezhi02@gmail.com>
2022-04-01 21:18:04 +0700

    membuat view Login

---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+<title>Hello, world!</title>
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-header text-center">
+                        <h1>Form Login</h1>
+                    </div>
+                    <div class="card-body">
+                        <form action="/login" method="POST">
+                        @csrf
+                        <div class="form-group mb-2">
+                            <label for="email">Email</label>
+                            <input class="form-control" type="email" id="email" placeholder="Email">
+                        </div>
+                        <div class="form-group mb-2">
+                            <label for="password">Password</label>
+                            <input class="form-control" type="password" id="password" placeholder="Password">
+                        </div>
+                        <div class="col-5 mt-3">
+                            <button type="submit" class="btn btn-primary">Login</button>
+                        </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
